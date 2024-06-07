b3aff130077847ce0546d013404b0d93ea64e2c8
Jordan M <jormmiller@gmail.com>
2024-06-07 13:13:38 -0400

    changed db connection string

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost/social-network-api';
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.01:27017/socialNetworkDB';
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
